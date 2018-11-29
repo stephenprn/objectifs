bf7496aded915d9b4b4fa4e010e40abd8c2dcdcf
stephenprn <stephen.perrin@2017.icam.fr>
2018-11-29 11:10:27 +0100

    Auto-complete implemented + fix suggestion service getResults()

--- src/pages/addObjectif/addObjectif.ts
+++ src/pages/addObjectif/addObjectif.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ViewController } from 'ionic-angular';
 import { ObjectifsService } from '../../services/objectifs.service';
 import { AppConstants } from '../../app/app.constants';
@@ -6,19 +6,23 @@ import { FormBuilder, Validators } from '@angular/forms';
 import * as _ from 'lodash';
 import { DateService } from '../../services/date.service';
 import { Objectif } from '../../models/objectif.model';
+import { SuggestionsService } from '../../services/suggestions.service';
+import { AutoCompleteComponent } from 'ionic2-auto-complete';
 
 @Component({
   selector: 'page-add-objectif',
   templateUrl: 'addObjectif.html'
 })
 export class AddObjectifPage {
+  @ViewChild('autocomplete') autocomplete: AutoCompleteComponent;
+  
   colors: any[] = [];
   formGroup: any;
   submitAttempted: boolean = false;
 
   constructor(public viewCtrl: ViewController, public formBuilder: FormBuilder,
-    private objectifsService: ObjectifsService, private dateService: DateService) {
-    //Initial color value: blue
+    private objectifsService: ObjectifsService, private dateService: DateService, public suggestionsService: SuggestionsService) {
+    // Initial color value: blue
     this.formGroup = formBuilder.group({
       title: ['', [Validators.required]],
       date: ['', [Validators.required]],
@@ -28,6 +32,9 @@ export class AddObjectifPage {
     });
 
     this.colors = AppConstants.colors;
+
+    // // Set focus on the auto-focus at the init of the page
+    // this.autocomplete.setFocus();
   }
 
   dismiss(): void {
@@ -35,9 +42,12 @@ export class AddObjectifPage {
   }
 
   submit(): void {
+    this.formGroup.patchValue({
+      title: this.autocomplete.keyword
+    });
+
     if (!this.formGroup.valid) {
       this.submitAttempted = true;
-      console.log('not valid');
       return;
     }
 
